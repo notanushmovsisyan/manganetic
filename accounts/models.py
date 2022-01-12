65f88a0a004b78ffb629107b82c8277e55c731a0
Anush Movsisyan <anush.movsisyan1902@gmail.com>
2022-01-12 13:40:39 +0400

    Created User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser, PermissionsMixin
+)
+from accounts.managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = AbstractUser.username
+    email = models.EmailField(max_length=254, unique=True)
+    #  bookmarks = models.ForeignKey(Manga, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
